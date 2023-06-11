a8a6a6faf2e00f2181439e8202cac679cc430c5e
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-12 01:29:31 +0300

    Add social providers as enums

--- src/main/java/com/example/SpringAngularAuth/dto/SocialProvider.java
+++ src/main/java/com/example/SpringAngularAuth/dto/SocialProvider.java
@@ -1,4 +1,16 @@
 package com.example.SpringAngularAuth.dto;
 
-public class SocialProvider {
+public enum SocialProvider {
+
+    FACEBOOK("facebook"), TWITTER("twitter"), LINKEDIN("linkedin"), GOOGLE("google"), GITHUB("github"), LOCAL("local");
+
+    private String providerType;
+
+    public String getProviderType() {
+        return providerType;
+    }
+
+    SocialProvider(final String providerType) {
+        this.providerType = providerType;
+    }
 }
