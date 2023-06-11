811a3ab7e31feee9c275ed4c27a46d21c98c002f
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-12 01:21:04 +0300

    Add functionality to loginRequest

--- src/main/java/com/example/SpringAngularAuth/dto/LoginRequest.java
+++ src/main/java/com/example/SpringAngularAuth/dto/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.example.SpringAngularAuth.dto;
 
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
 public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
