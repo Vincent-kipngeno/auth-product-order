13fb409b7f1a00da9b7853e32dbb3ca51ee7b4e9
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 20:34:19 +0300

    Add LocalUserDetailsService that tries to load a user by their username in order to get the LocalUser

--- src/main/java/com/example/SpringAngularAuth/service/LocalUserDetailService.java
+++ src/main/java/com/example/SpringAngularAuth/service/LocalUserDetailService.java
@@ -1,4 +1,26 @@
 package com.example.SpringAngularAuth.service;
 
-public class LocalUserDetailService {
+import com.example.SpringAngularAuth.dto.LocalUser;
+import com.example.SpringAngularAuth.model.User;
+import com.example.SpringAngularAuth.util.GeneralUtils;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("localUserDetailService")
+public class LocalUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Transactional
+    public LocalUser loadUserByUsername(final String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + email + " was not found in the database");
+        }
+        return new LocalUser(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, GeneralUtils.buildSimpleGrantedAuthorities(user.getRoles()), user);
+    }
 }
