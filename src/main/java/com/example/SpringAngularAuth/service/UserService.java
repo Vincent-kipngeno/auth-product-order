acf0ff52d9cbe51eeabbae0126cfc097e804dcdd
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 16:17:52 +0300

    Add an interface for user service

--- src/main/java/com/example/SpringAngularAuth/service/UserService.java
+++ src/main/java/com/example/SpringAngularAuth/service/UserService.java
@@ -1,4 +1,16 @@
 package com.example.SpringAngularAuth.service;
 
-public class UserService {
+import com.example.SpringAngularAuth.dto.LocalUser;
+import com.example.SpringAngularAuth.dto.UserRegistrationForm;
+import com.example.SpringAngularAuth.exception.UserAlreadyExistAuthenticationException;
+import com.example.SpringAngularAuth.model.User;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+
+import java.util.Map;
+
+public interface UserService {
+    public User registerNewUser(UserRegistrationForm UserRegistrationForm) throws UserAlreadyExistAuthenticationException;
+    User findUserByEmail(String email);
+    LocalUser processUserRegistration(String registrationId, Map<String, Object> attributes, OidcIdToken idToken, OidcUserInfo userInfo);
 }
