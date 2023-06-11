d1026c7d723e0b368744ce7d4d9162f71adfc4f0
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 20:50:23 +0300

    Create a UserInfo POJO class to act as a dummy entity for stored procedure

---
+++ src/main/java/com/example/SpringAngularAuth/model/UserInfo.java
@@ -0,0 +1,33 @@
+package com.example.SpringAngularAuth.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@SqlResultSetMapping(name = "UserInfo", classes = @ConstructorResult(targetClass = UserInfo.class, columns = { @ColumnResult(name = "id", type = String.class),
+        @ColumnResult(name = "name", type = String.class) }))
+public class UserInfo {
+    @Id
+    private String id;
+    private String name;
+
+    public UserInfo(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
