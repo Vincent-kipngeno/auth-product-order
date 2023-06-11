5a2ba05868743afc8739a2ba89228895702a2ab8
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 16:06:51 +0300

    Add custom UserRepository interface

--- src/main/java/com/example/SpringAngularAuth/repo/UserRepository.java
+++ src/main/java/com/example/SpringAngularAuth/repo/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.SpringAngularAuth.repo;
 
-public class UserRepository {
+import com.example.SpringAngularAuth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    boolean existsByEmail(String email);
 }
