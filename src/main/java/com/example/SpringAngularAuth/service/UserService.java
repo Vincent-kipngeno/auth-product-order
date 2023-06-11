ac5c6ca57309ffc82d0d7b25591ff8336248f076
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-12 00:40:58 +0300

    Add an OPTIONAL interface function for finding user by id

--- src/main/java/com/example/SpringAngularAuth/service/UserService.java
+++ src/main/java/com/example/SpringAngularAuth/service/UserService.java
@@ -6,11 +6,13 @@ import com.example.SpringAngularAuth.exception.UserAlreadyExistAuthenticationExc
 import com.example.SpringAngularAuth.model.User;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import java.util.Optional;
 
 import java.util.Map;
 
 public interface UserService {
     public User registerNewUser(UserRegistrationForm UserRegistrationForm) throws UserAlreadyExistAuthenticationException;
     User findUserByEmail(String email);
+    Optional<User> findUserById(Long id);
     LocalUser processUserRegistration(String registrationId, Map<String, Object> attributes, OidcIdToken idToken, OidcUserInfo userInfo);
 }
