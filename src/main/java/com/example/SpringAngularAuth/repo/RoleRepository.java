c5c62dd4c1d29051b67a85649022dae4e6362acf
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 16:08:37 +0300

    Add custom RoleRepository interface

--- src/main/java/com/example/SpringAngularAuth/repo/RoleRepository.java
+++ src/main/java/com/example/SpringAngularAuth/repo/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.example.SpringAngularAuth.repo;
 
-public class RoleRepository {
+import com.example.SpringAngularAuth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
