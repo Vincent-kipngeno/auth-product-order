a12bbc08814a5a4ea349975d74ad09892680e568
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 16:14:59 +0300

    Add general utils to aid in getting the used socialProvider or in building simpleGrantedAAuthorities based on roles

--- src/main/java/com/example/SpringAngularAuth/util/GeneralUtils.java
+++ src/main/java/com/example/SpringAngularAuth/util/GeneralUtils.java
@@ -1,4 +1,29 @@
 package com.example.SpringAngularAuth.util;
 
+import com.example.SpringAngularAuth.dto.SocialProvider;
+import com.example.SpringAngularAuth.model.Role;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class GeneralUtils {
+
+    public static List<SimpleGrantedAuthority> buildSimpleGrantedAuthorities(final Set<Role> roles) {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    public static SocialProvider toSocialProvider(String providerId) {
+        for (SocialProvider socialProvider : SocialProvider.values()) {
+            if (socialProvider.getProviderType().equals(providerId)) {
+                return socialProvider;
+            }
+        }
+        return SocialProvider.LOCAL;
+    }
 }
