85e16a6f0961c96de84ea9cf6d81c9b676ec3eca
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-11 16:03:30 +0300

    Create Role model class/entity

--- src/main/java/com/example/SpringAngularAuth/model/Role.java
+++ src/main/java/com/example/SpringAngularAuth/model/Role.java
@@ -1,4 +1,93 @@
 package com.example.SpringAngularAuth.model;
 
-public class Role {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public static final String USER = "USER";
+    public static final String ROLE_USER = "ROLE_USER";
+
+    @Id
+    @Column(name = "ROLE_ID")
+    private int roleId;
+
+    private String name;
+
+    // bi-directional many-to-many association to User
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters goes here
+
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role role = (Role) obj;
+        if (!role.equals(role.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Role [name=").append(name).append("]").append("[id=").append(roleId).append("]");
+        return builder.toString();
+    }
 }
