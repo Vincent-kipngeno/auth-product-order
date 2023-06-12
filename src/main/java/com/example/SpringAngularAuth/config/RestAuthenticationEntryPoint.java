2dfea4b2b8c52ee9e8bc99ce40203e0fd155755e
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-12 14:54:46 +0300

    Add webSecurityConfiguration to create a security filter chain

--- src/main/java/com/example/SpringAngularAuth/config/RestAuthenticationEntryPoint.java
+++ src/main/java/com/example/SpringAngularAuth/config/RestAuthenticationEntryPoint.java
@@ -1,4 +1,24 @@
 package com.example.SpringAngularAuth.config;
 
-public class RestAuthenticationEntryPoint {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getLocalizedMessage());
+    }
 }
