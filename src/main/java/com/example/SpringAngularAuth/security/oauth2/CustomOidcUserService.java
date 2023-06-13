5e0a3c7475daf6753e5b4bbf1b2c57083921dd17
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-13 09:24:53 +0300

    Add oidcUser service to fetch user details from an oauth2UserService that supports OpenID Connect like Googlke

--- src/main/java/com/example/SpringAngularAuth/security/oauth2/CustomOidcUserService.java
+++ src/main/java/com/example/SpringAngularAuth/security/oauth2/CustomOidcUserService.java
@@ -1,4 +1,42 @@
 package com.example.SpringAngularAuth.security.oauth2;
 
-public class CustomOidcUserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+import com.example.SpringAngularAuth.exception.OAuth2AuthenticationProcessingException;
+import com.example.SpringAngularAuth.service.UserService;
+
+/**
+ * OidcUserService is an implementation of an OAuth2UserService that supports OpenID Connect 1.0 Providers. Google is an OpenID Connect provider. Hence, we are creating this service to load the user with Google’s OAuth 2.0 APIs.
+ *
+ * The CustomOidcUserService extends Spring Security’s OidcUserService and implements its loadUser() method. This method is called after an access token is obtained from the OAuth2 provider.
+ *
+ * In this method, we first fetch the user’s details from the OAuth2 provider. If a user with the same email already exists in our database then we update his details, otherwise, we register a new user.
+ */
+@Service
+public class CustomOidcUserService extends OidcUserService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+        try {
+            return userService.processUserRegistration(userRequest.getClientRegistration().getRegistrationId(), oidcUser.getAttributes(), oidcUser.getIdToken(),
+                    oidcUser.getUserInfo());
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            // Throwing an instance of AuthenticationException will trigger the
+            // OAuth2AuthenticationFailureHandler
+            throw new OAuth2AuthenticationProcessingException(ex.getMessage(), ex.getCause());
+        }
+    }
 }
