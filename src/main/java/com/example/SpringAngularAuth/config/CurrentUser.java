e3ad400fe3f6dc450426793cefbe0a52da86f20d
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-15 20:40:56 +0300

    Add a current user interface which is a meta-annotation that can be used to inject the currently authenticated user principal in the controllers

--- src/main/java/com/example/SpringAngularAuth/config/CurrentUser.java
+++ src/main/java/com/example/SpringAngularAuth/config/CurrentUser.java
@@ -1,4 +1,19 @@
 package com.example.SpringAngularAuth.config;
 
-public class CurrentUser {
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+/**
+ * CurrentUser is a meta-annotation that can be used to inject the currently authenticated user principal in the controllers
+ */
+@Target({ ElementType.PARAMETER, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
 }
