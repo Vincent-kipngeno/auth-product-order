476747880f8d0315f84cc5eee2c7628f77353635
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-15 20:46:30 +0300

    Update the main class to extend the SpringBootServletInitializer

--- src/main/java/com/example/SpringAngularAuth/SpringAngularAuthApplication.java
+++ src/main/java/com/example/SpringAngularAuth/SpringAngularAuthApplication.java
@@ -2,12 +2,23 @@ package com.example.SpringAngularAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-public class SpringAngularAuthApplication {
+@SpringBootApplication(scanBasePackages = "com.example.SpringAngularAuth")
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class SpringAngularAuthApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAngularAuthApplication.class, args);
+		SpringApplicationBuilder app = new SpringApplicationBuilder(SpringAngularAuthApplication.class);
+		app.run();
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringAngularAuthApplication.class);
+	}
 }
