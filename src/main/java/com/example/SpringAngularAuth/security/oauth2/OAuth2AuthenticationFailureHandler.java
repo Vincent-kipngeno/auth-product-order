576573a4ad42b45470e70a2dc91d4fc54be83d53
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-15 19:59:07 +0300

    Add OAuth2UserInfo class that maps details from each of the oauth 2 apps

--- src/main/java/com/example/SpringAngularAuth/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/example/SpringAngularAuth/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -1,4 +1,45 @@
 package com.example.SpringAngularAuth.security.oauth2;
 
-public class OAuth2AuthenticationFailureHandler {
+import static com.example.SpringAngularAuth.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.example.SpringAngularAuth.util.CookieUtils;
+
+/**
+ * On Authentication failure, Spring Security invokes the onAuthenticationFailure() method of the OAuth2AuthenticationFailureHandler that we have configured in WebSecurityConfig.
+ *
+ * This method,
+ *
+ * Fetches the client redirect_uri from the cookie
+ * Removes Authorization request cookie as well ass redirect_uri cookie
+ * Redirects the user to the angular client with the error message added to the query string
+ */
+@Component
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Autowired
+    HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue).orElse(("/"));
+
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl).queryParam("error", exception.getLocalizedMessage()).build().toUriString();
+
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
 }
