48d8bf158c334891ad087871c11823b9a71ab837
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-15 20:03:04 +0300

    Add class containing user info from Linkedin

--- src/main/java/com/example/SpringAngularAuth/security/oauth2/user/LinkedinOAuth2UserInfo.java
+++ src/main/java/com/example/SpringAngularAuth/security/oauth2/user/LinkedinOAuth2UserInfo.java
@@ -1,4 +1,30 @@
 package com.example.SpringAngularAuth.security.oauth2.user;
 
-public class LinkedinOAuth2UserInfo {
+import java.util.Map;
+
+public class LinkedinOAuth2UserInfo extends OAuth2UserInfo {
+
+    public LinkedinOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getName() {
+        return ((String) attributes.get("localizedFirstName")).concat(" ").concat((String) attributes.get("localizedLastName"));
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("emailAddress");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("pictureUrl");
+    }
 }
