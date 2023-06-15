c91ce47426f855af80ebaaeca1d6f8c9fbf4adc1
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-15 20:20:51 +0300

    Add a user controller class that exposes API’s for fetching public content as well as user role specific contents

--- src/main/java/com/example/SpringAngularAuth/controller/UserController.java
+++ src/main/java/com/example/SpringAngularAuth/controller/UserController.java
@@ -1,4 +1,48 @@
 package com.example.SpringAngularAuth.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringAngularAuth.config.CurrentUser;
+import com.example.SpringAngularAuth.dto.LocalUser;
+import com.example.SpringAngularAuth.util.GeneralUtils;
+
+/**
+ * This controller exposes API’s for fetching public content as well as user role specific contents
+ */
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> getCurrentUser(@CurrentUser LocalUser user) {
+        return ResponseEntity.ok(GeneralUtils.buildUserInfo(user));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getContent() {
+        return ResponseEntity.ok("Public content goes here");
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> getUserContent() {
+        return ResponseEntity.ok("User content goes here");
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getAdminContent() {
+        return ResponseEntity.ok("Admin content goes here");
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public ResponseEntity<?> getModeratorContent() {
+        return ResponseEntity.ok("Moderator content goes here");
+    }
 }
