8673693605087e9c799b32649c4d598548c80d90
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-15 20:16:30 +0300

    Add auth controller class that exposes two post endpoints for sign in and sign up

--- src/main/java/com/example/SpringAngularAuth/controller/AuthController.java
+++ src/main/java/com/example/SpringAngularAuth/controller/AuthController.java
@@ -1,4 +1,65 @@
 package com.example.SpringAngularAuth.controller;
 
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringAngularAuth.dto.ApiResponse;
+import com.example.SpringAngularAuth.dto.JwtAuthenticationResponse;
+import com.example.SpringAngularAuth.dto.LocalUser;
+import com.example.SpringAngularAuth.dto.LoginRequest;
+import com.example.SpringAngularAuth.dto.SignUpRequest;
+import com.example.SpringAngularAuth.exception.UserAlreadyExistAuthenticationException;
+import com.example.SpringAngularAuth.security.jwt.TokenProvider;
+import com.example.SpringAngularAuth.service.UserService;
+import com.example.SpringAngularAuth.util.GeneralUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This controller exposes 2 POST API’s for User Login and Registration requests
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    TokenProvider tokenProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.createToken(authentication);
+        LocalUser localUser = (LocalUser) authentication.getPrincipal();
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt, GeneralUtils.buildUserInfo(localUser)));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        try {
+            userService.registerNewUser(signUpRequest);
+        } catch (UserAlreadyExistAuthenticationException e) {
+            log.error("Exception Ocurred", e);
+            return new ResponseEntity<>(new ApiResponse(false, "Email Address already in use!"), HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok().body(new ApiResponse(true, "User registered successfully"));
+    }
 }
